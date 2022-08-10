93996f24a733b0412c45a2537cdf4c94a7a5b7b4
Kate <kate.burovova@gmail.com>
2022-08-10 16:30:03 +0300

    minor updates

--- scrapping.py
+++ scrapping.py
@@ -1,7 +1,7 @@
 import twint
 c = twint.Config()
 c.Search = ['русня'] # topic
-c.Limit = 100 # number of Tweets to scrape
+c.Limit = 500 # number of Tweets to scrape
 c.Store_csv = True # store tweets in a csv file
 c.Output = 'data/tweets.csv' # path to csv file
 twint.run.Search(c)
